64bd683b82f878dbd2bef132fbe65cdc1c8a7a9f
Kml159 <99826221+Kml159@users.noreply.github.com>
2023-11-28 09:43:07 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class process{
+    
+    private:
+
+        string name;                // process name (P1, P2, etc.)
+        unsigned int missionTime;   // 
+};
+
+int main(){
+
+
+
+    return 0;
+}
